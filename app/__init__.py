e42f7641451eb5ac9ce6985e5c32b6810ec34f6d
Jason Chan <jchan61@stuy.edu>
2026-04-01 11:39:08 -0400

    made login and register page but still neeeeeeeds some adjusting

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,73 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+from flask import redirect, url_for
+
+import sqlite3   #enable control of an sqlite database
+import datetime
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+###############################################
+#maya you got that
+###############################################
+c.execute("CREATE TABLE IF NOT EXISTS users (name TEXT NOT NULL COLLATE NOCASE, bio TEXT, password TEXT NOT NULL, UNIQUE(name))")	# creates table
+c.execute("CREATE TABLE IF NOT EXISTS stories (story_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, last_update DATE, author_name TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS edits (edit_id INTEGER PRIMARY KEY AUTOINCREMENT, story_id INTEGER, author_name TEXT, content TEXT)")
+
+app = Flask(__name__)
+app.secret_key = "secret"
+
+
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # store username and password as a variable
+        username = request.form.get('username').strip().lower()
+        password = request.form.get('password').strip()
+
+        # render login page if username or password box is empty
+        if not username or not password:
+            return render_template('login.html', error="No username or password inputted")
+
+        #search user table for password from a certain username
+        db = sqlite3.connect(DB_FILE)
+        c = db.cursor()
+        account = c.execute("SELECT password FROM users WHERE name = ?", (username,)).fetchone()
+        db.close()
+
+        #if there is no account then reload page
+        if account is None:
+            return render_template("login.html", error="Username or password is incorrect")
+
+        # check if password is correct, if not then reload page
+        if account[0] != password:
+            return render_template("login.html", error="Username or password is incorrect")
+
+        # if password is correct redirect home
+        session["username"] = username
+        return redirect(url_for("home"))
+
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None) # remove username from session
+    return redirect(url_for('login'))
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug = True, host='0.0.0.0')
